36f634428d843c8b846c4576d8bb79abde995ba6
karol-121 <36259145+karol-121@users.noreply.github.com>
2023-01-24 19:37:51 +0100

    Added wrap/nowrap, textarea reset & gui fix
    Now it is possible to toggle between wrap/nowrap in textarea, there is also button for resetting textarea and gui has been refreshed

--- src/gui/page.js
+++ src/gui/page.js
@@ -1,63 +1,4 @@
-//handle communication with background script here.
-//have to use messages as this is the only way to get communication with the background script in incognito mode
-browser.runtime.onMessage.addListener(handleMessage);
-
-function handleMessage(request, sender, response) {
-  
-  //care only about messages whose status is "fulfilled"
-	if (request.status === "fulfilled") {
-
-		if (request.action === "get") {
-   		handle_get(request.param);
-  	}
-
-  	if (request.action === "open") {
-   		handle_open();
-  	}
-
-	}
-}
-
-const textarea = document.getElementById("text");
-let lastTextareaValue = "";
-
-//"get" button has been clicked
-document.getElementById("get_button").addEventListener("click", function(e) {
-
-	//initiate a get-request for background worker
-	browser.runtime.sendMessage({action: "get", status: "initiated"});
-
-});
-
-//"open" button has been clicked
-document.getElementById("open_button").addEventListener("click", function(e) {
-
-	//get text from textarea
-	let textareaValue = textarea.value;
-
-	//check if current list/text does match with previous get, if so do not execute
-	//otherwise it will be possible to repeatedly open the same set of tabs, get print list of tabs that is possible to open	
-	if (textareaValue != lastTextareaValue) {
-
-		//retrive valid urls from text
-		const urls = new Array();
-		const values = textareaValue.split(/\s/gm);
-
-		for (value of values) {
-
-			let url = fixUrl(value);
-			urls.push(url);
-
-		}
-
-		//initiate a open-request for background worker
-		browser.runtime.sendMessage({action: "open", status: "initiated", param: urls});
-
-	}
-
-});
-
-//function that fixes urls (making them valid)
+//function that fixes URLs (making them valid)
 function fixUrl(value) {
 	const startWithProtocol = /\S{1,5}:\/\/\S*/gm; //match values that begins with protocol
 	const defaultProtocol = "https://"; //protocol to append if link does not contain such
@@ -73,18 +14,15 @@ function fixUrl(value) {
 	}
 
 	return value;
-
 }
 
-
 //defines actions that happens after open-request is fulfilled 
-function handle_open() {
+function handleOpen() {
 	textarea.value = "";
 }
 
 //defines actions that happens after get-request is fulfilled
-function handle_get(urls) {
-
+function handleGet(urls) {
 	//print all urls, each in new line to textarea:
 	let text = "";
 
@@ -95,6 +33,78 @@ function handle_get(urls) {
 
 	lastTextareaValue = text;
 	textarea.value = text;
-
 }
+
+//function that handles messages
+function handleMessage(request, sender, response) {
+  
+  //care only about messages whose status is "fulfilled"
+	if (request.status === "fulfilled") {
+
+		if (request.action === "get") {
+   		handleGet(request.param);
+  	}
+
+  	if (request.action === "open") {
+   		handleOpen();
+  	}
+
+	}
+}
+
+
+//entry point
+//this is where script starts executing from
+
+const textarea = document.getElementById("text");
+const wrapCheckbox = document.getElementById("wrap");
+let lastTextareaValue = "";
+
+//handle communication with background script here.
+//have to use messages as this is the only way to get communication with the background script in incognito mode
+browser.runtime.onMessage.addListener(handleMessage);
+
+//"wrap" checkbox has been clicked
+wrapCheckbox.addEventListener("click", function(e) {
+	if (wrapCheckbox.checked) {
+		textarea.wrap = "on";
+	} else {
+		textarea.wrap = "off";
+	}
+});
+
+document.getElementById("reset_button").addEventListener("click", function(e) {
+	textarea.value = "";
+});
+
+//"get" button has been clicked
+document.getElementById("get_button").addEventListener("click", function(e) {
+	//initiate a get-request for background worker
+	browser.runtime.sendMessage({action: "get", status: "initiated"});
+});
+
+//"open" button has been clicked
+document.getElementById("open_button").addEventListener("click", function(e) {
+	//get text from textarea
+	let textareaValue = textarea.value;
+
+	//check if current list/text does match with previous get, if so do not execute
+	//otherwise it will be possible to repeatedly open the same set of tabs, get print list of tabs that is possible to open	
+	if (textareaValue != lastTextareaValue) {
+
+		//retrieve and fix URLs from text
+		const urls = new Array();
+		const values = textareaValue.split(/\s/gm);
+
+		for (value of values) {
+
+			let url = fixUrl(value);
+			urls.push(url);
+
+		}
+
+		//initiate a open-request for background worker
+		browser.runtime.sendMessage({action: "open", status: "initiated", param: urls});
+	}
+});
 
